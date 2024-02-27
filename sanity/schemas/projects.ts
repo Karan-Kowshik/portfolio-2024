f1dd5206919324e9cf13a635810edbd22c5b09ac
Sriram <sgcuber24@gmail.com>
2024-02-27 17:02:14 +0530

    Init tailwind and sanity

---
+++ sanity/schemas/projects.ts
@@ -0,0 +1,19 @@
+const projects = {
+	name: "projects",
+	type: "document",
+	title: "Projects",
+	fields: [
+		{
+			name: "name",
+			type: "string",
+			title: "Name",
+		},
+		{
+			name: "description",
+			type: "string",
+			title: "Description",
+		},
+	],
+};
+
+export default projects;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,62 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "selector",
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				purple: {
+					50: "#ECF0FF",
+					100: "#DDE3FF",
+					200: "#C2CBFF",
+					300: "#9CA7FF",
+					400: "#7578FF",
+					500: "#635BFF",
+					600: "#4E36F5",
+					700: "#432AD8",
+					800: "#3725AE",
+					900: "#302689",
+					1000: "#1E1650",
+				},
+				neutral: {
+					50: "#F8FAFC",
+					100: "#F1F5F9",
+					200: "#E2E8F0",
+					300: "#CBD5E1",
+					400: "#94A3B8",
+					500: "#64748B",
+					600: "#475569",
+					700: "#334155",
+					800: "#1E293B",
+					900: "#0F172A",
+					1000: "#020617",
+				},
+				grey: {
+					50: "#FAFAFA",
+					100: "#F5F5F5",
+					200: "#E5E5E5",
+					300: "#D4D4D4",
+					400: "#A3A3A3",
+					500: "#737373",
+					600: "#525252",
+					700: "#404040",
+					800: "#262626",
+					900: "#171717",
+					1000: "#0A0A0A",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
